88d2facf5045dfe8260d8168b6061ba334f48786
preet singhwal <preet.singhwal@tothenew.com>
2024-10-17 01:31:34 +0530

    start making quiz application added json file, startscreen, created initial structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from './StartScreen';
+
+const intialState = {
+  questions: [],
+  // loading, ready, error, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [{questions, status}, dispatch] = useReducer(reducer, intialState);
+  const numQuestion = questions.length;
+  
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((resp) => resp.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFaileds" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter/>
+    <div className="app">
+      <Header></Header>
+      <Main>
+        {status === 'loading' && <Loader/>}
+        {status === 'error' && <Error/>}
+        {status === 'ready' && <StartScreen numQuestion={numQuestion}/>}
+      </Main>
     </div>
-  )
+  );
 }
